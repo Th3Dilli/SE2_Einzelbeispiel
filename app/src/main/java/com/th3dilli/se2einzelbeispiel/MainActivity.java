8024f3cf0cc9849657f87a7f934c3a949baf6e0c
th3dilli <th3dilli@gmx.at>
2020-03-09 10:52:54 +0100

    add initial layout

--- app/src/main/java/com/th3dilli/se2einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/th3dilli/se2einzelbeispiel/MainActivity.java
@@ -3,12 +3,19 @@ package com.th3dilli.se2einzelbeispiel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText matNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    void sendData(android.view.View view){
+
     }
 }
